859c92b2155121b6897500babbd65fe375e7ac36
jjohayeon <80299212+jjohayeon@users.noreply.github.com>
2021-12-05 22:52:28 +0900

    Add files via upload

--- hayeonvs/var_tabmenu.js
+++ hayeonvs/var_tabmenu.js
@@ -1,13 +1,61 @@
+//tab-button
 
+// $(".tab-button")
+//   .eq(0)
+//   .click(function () {
+//     $(".tab-button").removeClass("active");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-button").eq(0).addClass("active");
+//     $(".tab-content").eq(0).addClass("show");
+//   });
 
+// $(".tab-button")
+//   .eq(1)
+//   .click(function () {
+//     $(".tab-button").removeClass("active");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-button").eq(1).addClass("active");
+//     $(".tab-content").eq(1).addClass("show");
+//   });
+// $(".tab-button")
+//   .eq(2)
+//   .click(function () {
+//     $(".tab-button").removeClass("active");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-button").eq(2).addClass("active");
+//     $(".tab-content").eq(2).addClass("show");
+//   });
 
-//첫번째 버튼을 누르면
-//1. 첫번째 버튼에 active 클래스 추가
-//2. 첫번째 내용에 show라는 클래스 추가
+//for문으로 축약
+// var tabBtn = $(".tab-button").length;
+// for (let i = 0; i < tabBtn; i++) {
+//   //for문에서 var로 변수선언하면 실행안됨
 
-$('.tab-button').eq(0).click(function(){
-    $('.tab-button').removeClass('active');
-    $('.tab-button').eq(0).addClass('active');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(0).addClass('show');
+//   //   $(".tab-button")
+//   //     .eq(i)
+//   //     .click(function () {
+//   //       $(".tab-button").removeClass("active");
+//   //       $(".tab-content").removeClass("show");
+//   //       $(".tab-button").eq(i).addClass("active");
+//   //       $(".tab-content").eq(i).addClass("show");
+//   //     });
+
+//   $(".tab-button")
+//     .eq(i)
+//     .click(function () {
+//       openTab(i);
+//     });
+// }
+
+//셀렉터.dataset.id js
+$(".list").click(function (e) {
+  openTab(e.target.dataset.id);
 });
+//함수로 축약해서 쓰기
+
+function openTab(num) {
+  $(".tab-button").removeClass("active");
+  $(".tab-content").removeClass("show");
+  $(".tab-button").eq(num).addClass("active");
+  $(".tab-content").eq(num).addClass("show");
+}
